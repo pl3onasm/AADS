a1df201669ddf847a45c63b2663378409be45ba3
David <pl3onasm@gmail.com>
2023-07-31 13:15:38 +0200

    :tada:

--- algorithms/sorting/bubble-sort/bubblesort.c
+++ algorithms/sorting/bubble-sort/bubblesort.c
@@ -1,6 +1,9 @@
 /* file: bubblesort.c
-* author: David De Potter
-* description: bubble sort
+   author: David De Potter
+   email: pl3onasm@gmail.com
+   license: MIT, see LICENSE file in repository root folder
+   description: bubble sort
+   time complexity: O(n^2)
 */
 
 #include <stdlib.h>
