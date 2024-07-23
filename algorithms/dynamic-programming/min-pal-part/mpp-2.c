5bca491fd3e76382da755418e92fa766a14e6adc
David <pl3onasm@gmail.com>
2024-07-23 11:35:22 +0200

    min pal partition

---
+++ algorithms/dynamic-programming/min-pal-part/mpp-2.c
@@ -0,0 +1,95 @@
+/* 
+  file: mpp-2.c
+  author: David De Potter
+  email: pl3onasm@gmail.com
+  license: MIT, see LICENSE file in repository root folder
+  description: minimal palindromic partitioning 
+    using top-down DP approach with memoization
+  time complexity: O(n^2)
+*/ 
+
+#include "../../../lib/clib/clib.h"
+#include <stdint.h>
+
+//===================================================================
+// Reconstructs an optimal palindromic partitioning of a given string
+void showCuts(string *S, size_t *cuts, size_t k) {
+  if (cuts[k] == k) {
+    for (size_t i = 0; i <= k; i++) 
+      printf("%c", charAt(S, i));
+  } else {
+    showCuts(S, cuts, cuts[k]);
+    printf(" | ");
+    for (size_t i = cuts[k] + 1; i <= k; i++) 
+      printf("%c", charAt(S, i));
+  }
+}
+
+//===================================================================
+// Precomputes a function returning a boolean value indicating
+// whether a substring of the input string is a palindrome
+bool **computePalindromes(string *S) {
+
+  CREATE_MATRIX(bool, pal, strLen(S), strLen(S), false);
+
+  for (size_t i = strLen(S); i--; )
+    for (size_t j = i; j < strLen(S); j++) 
+      if (i == j) pal[i][j] = true;
+      else if (charAt(S, i) == charAt(S, j)) 
+        pal[i][j] = j - i == 1 ? true : pal[i + 1][j - 1];
+      
+  return pal;
+}
+
+//===================================================================
+// Computes the minimal number of cuts required to partition a 
+// string into palindromes, using top-down DP with memoization
+size_t partition(string *S, size_t i, size_t *dp, size_t *cuts, 
+                 bool **isPalindrome) {
+
+  if (dp[i] != SIZE_MAX) 
+    return dp[i];
+  
+  if (isPalindrome[0][i]) {
+    cuts[i] = i;
+    return dp[i] = 0;
+  }
+
+  size_t minCuts = i + 1;
+
+  for (size_t j = 0; j < i; j++) {
+    if (isPalindrome[j + 1][i] 
+        && 1 + partition(S, j, dp, cuts, isPalindrome) < minCuts) {
+      minCuts = 1 + dp[j];
+      cuts[i] = j;
+    }
+  }
+
+  return dp[i] = minCuts;
+}
+
+//===================================================================
+
+int main() {
+
+  READ_STRING(S, '\n');
+
+  CREATE_ARRAY(size_t, dp, strLen(S), SIZE_MAX);
+  CREATE_ARRAY(size_t, cuts, strLen(S), 0);
+  
+  bool **isPalindrome = computePalindromes(S);
+
+  partition(S, strLen(S) - 1, dp, cuts, isPalindrome);
+
+  printf("Min cuts: %zu\n", dp[strLen(S) - 1]);
+  printf("An optimal partitioning:\n  ");
+  showCuts(S, cuts, strLen(S) - 1);
+  printf("\n");
+
+  FREE_MATRIX(isPalindrome, strLen(S));
+  freeString(S);
+  free(dp);
+  free(cuts);
+
+  return 0;
+}
