61f471f435c1383334a48ad24a2cce39f41fcf97
David <pl3onasm@gmail.com>
2024-09-20 10:42:09 +0200

    fix

--- algorithms/dynamic-programming/longest-increasing-sub/lis-4.c
+++ algorithms/dynamic-programming/longest-increasing-sub/lis-4.c
@@ -14,26 +14,20 @@
 size_t computeLis (int *arr, size_t len, size_t *table, 
                    size_t *parents) {
   
-  size_t subLen = 1;
-  for (size_t i = 1; i < len; i++) {
-    if (arr[i] > arr[table[subLen - 1]]) {
-        // no pile can be extended, create a new one and update
-      parents[i] = table[subLen - 1]; 
-      table[subLen++] = i;
-    } else {
-        // use binary search to find the pile to extend
-      size_t low = 0, high = subLen - 1;
-      while (low < high) {
-        size_t mid = low + (high - low) / 2;
-        if (arr[table[mid]] <= arr[i]) 
-          low = mid + 1;
-        else high = mid;
-      }
-        // update the table and parent array
-      table[low] = i;
-      if (low > 0) 
-        parents[i] = table[low - 1];
+  size_t subLen = 0;
+  for (size_t i = 0; i < len; i++) {
+      // use binary search to find the pile to extend
+    size_t low = 1, high = subLen;
+    while (low <= high) {
+      size_t mid = low + (1 + high - low) / 2;
+      if (arr[i] > arr[table[mid]]) low = mid + 1;
+      else high = mid - 1;
     }
+      // update the table and parent array
+    table[low] = i;
+    parents[i] = table[low - 1];
+      // create a new pile if needed
+    if (low > subLen) subLen = low;
   }
   return subLen;
 }
@@ -60,13 +54,13 @@ int main () {
 
   READ(int, arr, "%d", len);
 
-  CREATE_ARRAY(size_t, table, len, 0);
+  CREATE_ARRAY(size_t, table, len + 1, 0);
   CREATE_ARRAY(size_t, prev, len, 0);
 
   size_t subLen = computeLis(arr, len, table, prev);
 
   printf("Max length: %zu\nSubsequence:\n ", subLen);
-  reconstructLis(arr, prev, table[subLen - 1], subLen);
+  reconstructLis(arr, prev, table[subLen], subLen);
   
   free(prev); free(table); free(arr);
   return 0;
