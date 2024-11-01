5d15fd2fcea888c508864bcfec8e6ef9249dd161
David <pl3onasm@gmail.com>
2024-11-01 15:24:30 +0100

    fft

--- algorithms/fft/fftlib/fft.c
+++ algorithms/fft/fftlib/fft.c
@@ -3,7 +3,7 @@
   author: David De Potter
   email: pl3onasm@gmail.com
   license: MIT, see LICENSE file in repository root folder
-  description: FFT implementation for integer inputs
+  description: FFT implementation 
 */
 
 #include "../../../lib/clib.h"
